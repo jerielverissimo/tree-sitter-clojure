b5b8f9a008556d82eaffba196ee62a019a19c712
Jeriel Baptista Verissimo <jeriel.verissimo@nubank.com.br>
2025-07-07 13:06:20 -0300

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+//! This crate provides Clojure language support for the [tree-sitter][] parsing library.
+//!
+//! Typically, you will use the [LANGUAGE][] constant to add this language to a
+//! tree-sitter [Parser][], and then use the parser to parse some code:
+//!
+//! ```
+//! use tree_sitter::Parser;
+//!
+//! let code = r#"
+//! (defn double [x] (* x 2))
+//! "#;
+//! let mut parser = Parser::new();
+//! let language = tree_sitter_clojure::LANGUAGE;
+//! parser
+//!     .set_language(&language.into())
+//!     .expect("Error loading Clojure parser");
+//! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
+//! ```
+//!
+//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [tree-sitter]: https://tree-sitter.github.io/
+
+use tree_sitter_language::LanguageFn;
+
+unsafe extern "C" {
+    fn tree_sitter_clojure() -> *const ();
+}
+
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
+///
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_clojure) };
+
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+pub const NODE_TYPES: &str = include_str!("../grammar-src/src/node-types.json");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_can_load_grammar() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE.into())
+            .expect("Error loading Clojure parser");
+    }
+}
